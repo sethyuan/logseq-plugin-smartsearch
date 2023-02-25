6e95ce8ea350c75376988336e597cbafc06adb89
Seth <sethyuan@gmail.com>
2023-02-25 16:52:40 +0800

    fix: persist with the original content

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -27,9 +27,10 @@ export async function parseContent(content) {
 
 export async function persistBlockUUID(block) {
   if (block.properties?.id == null) {
+    const content = (await logseq.Editor.getBlock(block.uuid)).content
     await logseq.Editor.updateBlock(
       block.uuid,
-      `${block.content}\nid:: ${block.uuid}`,
+      `${content}\nid:: ${block.uuid}`,
     )
   }
 }
