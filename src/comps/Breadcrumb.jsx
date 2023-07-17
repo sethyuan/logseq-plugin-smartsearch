57020c0768f36393b09cb0753e24dc7e12fe919f
Seth <sethyuan@gmail.com>
2023-07-17 15:10:45 +0800

    fix: katex in breadcrumb

--- src/comps/Breadcrumb.jsx
+++ src/comps/Breadcrumb.jsx
@@ -3,9 +3,11 @@ export default function Breadcrumb({ segments }) {
     <span class="kef-ss-b-segs">
       {segments.map(({ label, name, uuid }, i) => (
         <>
-          <span key={uuid} className="kef-ss-b-label">
-            {label}
-          </span>
+          <span
+            key={uuid}
+            className="kef-ss-b-label"
+            dangerouslySetInnerHTML={{ __html: label }}
+          ></span>
           {i + 1 < segments.length && (
             <span class="kef-ss-b-spacer mx-2 opacity-50">➤</span>
           )}
